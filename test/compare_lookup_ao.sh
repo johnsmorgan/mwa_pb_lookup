29644865109f58d85a78e3b47a3a25d5ca1d25aa
John Morgan <john.morgan@icrar.org>
2019-07-04 12:29:03 +0800

    new test scripts

---
+++ test/compare_lookup_ao.sh
@@ -0,0 +1,4 @@
+ao-beam -2016 -allsky -name 0000000000 -delays 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+gen_fake_metafits.py
+ln -s 0000000000-xxr.fits 0000000000.fits
+../lookup_jones.py -f 149.76 0000000000 xxr.fits 0000000000 _lookup.fits --beam_path=../gleam_jones.hdf5
