afb2b4c9f3a191b9186966d422fd29495adf734c
John Morgan <john.morgan@icrar.org>
2019-08-07 20:39:24 +0800

    delete unused files

--- test/gen_fake_metafits.py
+++
@@ -1,8 +0,0 @@
-from astropy.io import fits
-hdu = fits.PrimaryHDU()
-hdu.header['LST'] = 0.0
-hdu.header['DELAYS'] = "0,"*16
-hdu.header['GRIDNUM'] = 0
-hdu.header['EXPOSURE'] = 0.0
-hdul = fits.HDUList([hdu])
-hdul.writeto("0000000000.metafits")
