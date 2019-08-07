c8536e7ff943f679413f0525a338a213396e540c
John Morgan <john.morgan@icrar.org>
2019-08-07 20:44:55 +0800

    update with all-zero image file and trimmed metafits file

--- test/compare_lookup_ao.sh
+++ test/compare_lookup_ao.sh
@@ -1,2 +1,2 @@
-ao-beam -2016 -allsky -proto 1147313992_121-132_proto.fits -m 1147313992.metafits
-../lookup_jones.py -f 161.9 1147313992 _121-132_proto.fits 1147313992 _lookup.fits --beam_path=/data/other/pb_lookup/gleam_jones.hdf5
+#ao-beam -2016 -allsky -proto 1147313992_121-132_proto.fits -m 1147313992.metafits
+../lookup_jones.py -f 161.9 1147313992 _121-132_proto.fits beam- _lookup.fits --beam_path=/data/other/pb_lookup/gleam_jones.hdf5
