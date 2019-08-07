9262158d9a29c5e52538adc4aab5f65564a45715
John Morgan <john.morgan@icrar.org>
2019-08-07 22:26:20 +0800

    also generate xx/yy beam

--- test/compare_lookup_ao.sh
+++ test/compare_lookup_ao.sh
@@ -1,2 +1,3 @@
 ao-beam -2016 -allsky -proto 1147313992_121-132_proto.fits -m 1147313992.metafits
 ../lookup_jones.py -f 161.9 1147313992 _121-132_proto.fits beam- _lookup.fits --beam_path=/data/other/pb_lookup/gleam_jones.hdf5
+../lookup_beam.py -f 161.9 1147313992 _121-132_proto.fits beam- _lookup.fits --beam_path=/data/other/pb_lookup/gleam_xx_yy.hdf5
