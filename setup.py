ff7e2efe4bdbec4f06793248743d47e3e9a95928
Tim Galvin <tgalvin@curtin.edu.au>
2020-10-20 17:18:08 +0800

    initial python setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+reqs = [
+    "astropy",
+    "numpy",
+    "scipy",
+    "matplotlib",
+    "hdf5",
+]
+
+setup(
+    name='mwa_pb_lookup',
+    version='1.0',
+    author="John Morgan",
+    description="This package provides an alternative, far quicker way of generating MWA primary beams compared with the standard mwa_pb code.",
+    url="https://github.com/johnsmorgan/mwa_pb_lookup",
+    long_description=open('README.md').read(),
+    packages=['mwa_pb_lookup',],
+    license='GNU General Public License v3.0',
+    requires=reqs,
+    scripts=["mwa_pb_lookup/lookup_beam.py",
+             "mwa_pb_lookup/lookup_jones.py"],
+)
